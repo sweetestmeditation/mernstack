0c46ba5930588e799ae17ea0d007d7bb8311df48
bngwanforbi <brendan.ngwanforbi@gmail.com>
2023-07-23 20:04:15 -0700

    Implemented CRUD operations for workouts API endpoints using Express.js Router.

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,39 +1,26 @@
 const express = require('express')
-const Workout = require('../models/WorkoutModel')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
 
 const router = express.Router()
 
 // Get all workouts
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
 // Get a single workout
-router.get('/:id',(req, res) => {
-    res.json({mssg: 'GET a single workout'})
-})
+router.get('/:id', getWorkout)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-    const {title, load, reps} = req.body
-
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
+router.post('/', createWorkout) 
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-    const {title, load, reps} = req.body
-    res.json({mssg: 'DELETE a workout'})
-})
-
+router.delete('/:id', deleteWorkout)
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
